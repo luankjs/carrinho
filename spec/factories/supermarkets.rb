2c128d9e0b6d163d1b163538d2cc948fb1093d52
Luan Santos <luankjs@gmail.com>
2016-02-26 23:27:37 -0300

    Tests Supermarket model

--- spec/factories/supermarkets.rb
+++ spec/factories/supermarkets.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :supermarket do
-    name "MyString"
+    name "MySupermarket"
   end
 
 end
--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Supermarket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid factory" do
+  	expect(FactoryGirl.create(:supermarket)).to be_valid
+  end
+
+  it "is invaid without name" do
+  	expect(FactoryGirl.build(:supermarket, name: nil)).to be_invalid
+  end
 end
