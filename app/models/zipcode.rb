4cb397c8a01756f44e14af9f160a966aba9c8208
Luan Santos <luankjs@gmail.com>
2016-02-27 00:18:15 -0300

    Adds relationship to zipcode and tests

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,3 +1,6 @@
 class Zipcode < ActiveRecord::Base
+	has_many :supermarket_zipcodes
+	has_many :supermarkets, through: :supermarket_zipcodes
+	
 	validates :number, presence: true, uniqueness: true, format: { with: /\d{5}-\d{3}/ }
 end
--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -1,21 +1,44 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Zipcode, type: :model do
-	it "has a valid factory" do
-		expect(FactoryGirl.create(:zipcode)).to be_valid
+	describe Zipcode, "Validation" do
+		it "has a valid factory" do
+			expect(FactoryGirl.create(:zipcode)).to be_valid
+		end
+
+		it "is invalid without number" do
+			expect(FactoryGirl.build(:zipcode, number: nil)).to be_invalid
+		end
+
+		it "is invalid when duplicate" do
+			FactoryGirl.create(:zipcode)
+
+			expect(FactoryGirl.build(:zipcode)).to be_invalid
+		end
+
+		it "is invalid when out format" do
+			expect(FactoryGirl.build(:zipcode, number: "abcde-fgh")).to be_invalid
+		end
 	end
 
-	it "is invalid without number" do
-		expect(FactoryGirl.build(:zipcode, number: nil)).to be_invalid
-	end
+	describe Zipcode, "Association" do
+		before do
+			@zipcode = FactoryGirl.build(:zipcode)
+			@supermarket = FactoryGirl.create(:supermarket)
+			@zipcode.supermarkets << @supermarket
+			@zipcode.save
+		end
 
-	it "is invalid when duplicate" do
-		FactoryGirl.create(:zipcode)
+		it "assings supermarket_zipcode was created" do
+			expect change{ SupermarketZipcode.count }.by(1)
+		end
 
-		expect(FactoryGirl.build(:zipcode)).to be_invalid
-	end
+		it "assings supermarket_zipcode zipcode is correct" do
+			expect(SupermarketZipcode.last.zipcode).to eq(@zipcode)
+		end
 
-	it "is invalid when out format" do
-		expect(FactoryGirl.build(:zipcode, number: "abcde-fgh")).to be_invalid
+	  it "returns correct supermarkets from zipcode" do
+	  	expect(@zipcode.supermarkets).to eq([@supermarket])
+	  end
 	end
 end
