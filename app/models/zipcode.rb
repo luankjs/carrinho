c4a10800b4af32cb37efee908357d98a4d386cc0
Luan Santos <luankjs@gmail.com>
2016-02-26 23:10:47 -0300

    Removes defaults message to format validation

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,3 +1,3 @@
 class Zipcode < ActiveRecord::Base
-	validates :number, presence: true, uniqueness: true, format: { with: /\d{5}-\d{3}/, message: "only allows letters" }
+	validates :number, presence: true, uniqueness: true, format: { with: /\d{5}-\d{3}/ }
 end
