7acead011e42c5017b513dd00b1fa3d6b781a8e2
Luan Santos <luankjs@gmail.com>
2016-02-27 00:40:52 -0300

    Adds Product validations and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
-  belongs_to :supermarket
+  belongs_to :supermarket, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0.0 }
+  validates :supermarket, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Product, "Validations" do
+  	it "is a valid factory" do
+  		expect(FactoryGirl.create(:product)).to be_valid
+  	end
+
+  	it "is invalid without name" do
+  		expect(FactoryGirl.build(:product, name: nil)).to be_invalid
+  	end
+  	
+  	it "is invalid without description" do
+  		expect(FactoryGirl.build(:product, description: nil)).to be_invalid
+  	end
+  	
+  	it "is invalid without price" do
+  		expect(FactoryGirl.build(:product, price: nil)).to be_invalid
+  	end
+
+  	it "is invalid when price is not a number" do
+  		expect(FactoryGirl.build(:product, price: "abc")).to be_invalid
+  	end
+
+  	it "is invalid when price is less than or equal 0" do
+  		expect(FactoryGirl.build(:product, price: -1.5)).to be_invalid
+  		expect(FactoryGirl.build(:product, price: 0.0)).to be_invalid
+  	end
+  	
+  	it "is invalid without supermarket" do
+  		expect(FactoryGirl.build(:product, supermarket: nil)).to be_invalid
+  	end
+  end
 end
