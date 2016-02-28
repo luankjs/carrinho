8a2455eea01365357f1e8ca340ec30e59f2f756b
Luan Santos <luankjs@gmail.com>
2016-02-28 10:42:35 -0300

    Returns products by supermarket and tests

--- app/controllers/supermarkets/products_controller.rb
+++ app/controllers/supermarkets/products_controller.rb
@@ -1,4 +1,12 @@
 class Supermarkets::ProductsController < ApplicationController
+	before_action :set_supermarket
+
   def index
+  	@products = @supermarket.products
+  end
+
+  private
+  def set_supermarket
+  	@supermarket = Supermarket.find(params[:supermarket_id])
   end
 end
--- spec/controllers/supermarkets/products_controller_spec.rb
+++ spec/controllers/supermarkets/products_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Supermarkets::ProductsController, type: :controller do
+	describe 'GET #index' do
+		before do
+			@supermarketA = FactoryGirl.create(:supermarket)
+			@supermarketB = FactoryGirl.create(:supermarket)
+			@productA = FactoryGirl.create(:product, supermarket: @supermarketA)
+			@productB = FactoryGirl.create(:product, supermarket: @supermarketB)
+		end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+		it "assigns to return correct product" do
+			get :index, supermarket_id: @supermarketA.id
 
+			expect(assigns(:products)).to eq([@productA])
+		end
+
+		it "assigns to load correct supermarket" do
+			get :index, supermarket_id: @supermarketB.id
+
+			expect(assigns(:supermarket)).to eq(@supermarketB)
+		end
+	end
 end
