af0df7e1ea27497edd0ef0a7d5280adf17e495af
Luan Santos <luankjs@gmail.com>
2016-02-27 23:42:05 -0300

    Supermarket#index to return supermarkets by zipcode and tests

--- app/controllers/supermarket_controller.rb
+++ app/controllers/supermarket_controller.rb
@@ -1,4 +1,13 @@
 class SupermarketController < ApplicationController
+	before_action :set_zipcode
+
   def index
+  	@supermarkets = Supermarket.all
+  	@supermarkets = @zipcode.supermarkets if @zipcode
+  end
+
+  private
+  def set_zipcode
+  	@zipcode = Zipcode.find_by_number(params[:zipcode]) if params[:zipcode]
   end
 end
--- spec/controllers/supermarket_controller_spec.rb
+++ spec/controllers/supermarket_controller_spec.rb
@@ -1,12 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe SupermarketController, type: :controller do
-
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  	before do
+      @supermarket = FactoryGirl.create(:supermarket)
+  		@supermarket2 = FactoryGirl.create(:supermarket, name: "Supermarket2")
+  		@zipcode = FactoryGirl.create(:zipcode)
+  		@supermarket.zipcodes << @zipcode
+  		@supermarket.save
+  	end
+
+  	context "when receive a zipcode" do
+  		it "assigns to return supermarkets from zipcode" do
+				post :index, zipcode: @zipcode.number
+
+				expect(assigns(:supermarkets)).to eq([@supermarket])
+			end
+
+      it "assigns to return correct quantity of supermarkets" do
+        post :index, zipcode: @zipcode.number
+        
+        expect(assigns(:supermarkets).count).to eq(1)
+      end
+
+      it "assigns to zipcode is set" do
+        post :index, zipcode: @zipcode.number
+
+        expect(assigns(:zipcode)).to eq(@zipcode)
+      end
+  	end
+
+    context "when not receive a zipcode" do
+      it "assigns to return supermarkets without zipcode" do
+        post :index
+
+        expect(assigns(:supermarkets)).to include(@supermarket2)
+      end
+
+      it "assigns to return correct quantity of supermarkets" do
+        post :index
+
+        expect(assigns(:supermarkets).count).to eq(2)
+      end
+
+      it "assigns to zipcode is nil" do
+        post :index
+
+        expect(assigns(:zipcode)).to eq(nil)
+      end
     end
   end
-
 end
