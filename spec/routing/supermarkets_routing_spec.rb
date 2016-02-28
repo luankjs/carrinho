d877dc824ccd04f151971c621ed157f329f0559f
Luan Santos <luankjs@gmail.com>
2016-02-27 23:05:47 -0300

    Fixes routing to Supermarket

--- spec/routing/supermarkets_routing_spec.rb
+++ spec/routing/supermarkets_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe SupermarketController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/supermarkets/59056-200").to route_to("supermarket#index", zipcode: "59056-200")
+      expect(post: "/supermarkets").to route_to("supermarket#index")
     end
   end
 end
