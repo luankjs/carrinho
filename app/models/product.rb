854e3ceedb420b1680224a8ceed2db2d466f4848
Luan Santos <luankjs@gmail.com>
2016-02-29 01:38:40 -0300

    Fixes default url to misses logo and photo

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,6 @@ class Product < ActiveRecord::Base
   validates :price, presence: true, numericality: { greater_than: 0.0 }
   validates :supermarket, presence: true
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
