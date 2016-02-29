029ca50a27ac200f8ba911df176e0c98d76dccb8
Luan Santos <luankjs@gmail.com>
2016-02-29 01:31:35 -0300

    Displays supermarkets by zipcode and tests

--- spec/views/supermarket/index.html.erb_spec.rb
+++ spec/views/supermarket/index.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "supermarket/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "supermarket/index.html.erb", type: :feature do
+  before do
+  	@zipcode = FactoryGirl.create(:zipcode)
+  	@supermarket = FactoryGirl.create(:supermarket)
+  	@supermarket.zipcodes << @zipcode
+  	@supermarket.save
+
+  	visit root_path
+
+  	fill_in :zipcode, with: @zipcode.number
+  	click_button I18n.t(:search)
+  end
+
+  it "assigns to render correct supermarket by zipcode" do
+  	expect(page).to have_content(@supermarket.name)
+  end
+
+  it "assigns to render num ber by zipcode" do
+  	expect(page).to have_content(@zipcode.number)
+  end
 end
